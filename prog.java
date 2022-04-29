a1e77744011b640a2a4669feea0157db003e315c
montu sharma <montu@example.com>
2022-04-29 20:12:02 +0530

    Testing Jenkins Job

--- prog.java
+++ prog.java
@@ -1,5 +1,15 @@
+class box{
+    
+    public box(String type){
+
+    }
+}
+
 public class prog{
+    enum box = ["Square","Rectangle"];
     public static void main(String args []){
-       System.out.println("First Jenkins Job");
+       System.out.println("First Jenkins Job -1 ");
+
+       
     }
 }
