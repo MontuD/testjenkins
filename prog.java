1c7dc635b3348ff6860c69d97a269b91d9b7b5a1
montu sharma <montu@example.com>
2022-04-29 17:24:36 +0530

    Output message added 5

--- prog.java
+++ prog.java
@@ -1,7 +1,5 @@
 public class prog{
     public static void main(String args []){
-        System.out.println("Hello World 23");
-        System.out.println("Hello World34536");
-        System.out.println("Hello World345fdf36");
+       System.out.println("First Jenkins Job");
     }
 }
